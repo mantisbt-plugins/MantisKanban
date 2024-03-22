04e2d578d5eb90488a33d99299d4815fc65a7606
Cas <cas@nuy.info>
2024-03-22 16:15:47 +0100

    Delete pages/group_delete_db.php

--- pages/group_delete_db.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-$reqVar = '_' . $_SERVER['REQUEST_METHOD'];
-$form_vars = $$reqVar;
-$delete_id = $form_vars['delete_id'] ;
-# Deleting definition
-# Need to add check on available data
-$query = "DELETE FROM {plugin_MantisKanban_kanbangroups} WHERE group_id = $delete_id";        
-db_query($query);
-print_header_redirect( 'plugin.php?page=MantisKanban/group_edit' );
